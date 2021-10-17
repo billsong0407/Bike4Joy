708dd06a7d5396df5de5918fe71925c1773d98c4
Bill Song <songchao456@gmail.com>
2021-10-17 19:39:57 -0400

    component rename

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <div className="container-fluid page-footer">
+            <footer className="pt-4">
+                <ul class="nav justify-content-center border-bottom pb-4">
+                    <li clasNames="nav-item">About</li>
+                    <li className="nav-item">Privacy Policy</li>
+                    <li className="nav-item">Social Media</li>
+                    <li className="nav-item">Help</li>
+                    <li className="nav-item">&copy; 2021 Bike4Joy</li>
+                </ul>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import { Button, Navbar, Nav, Container } from 'react-bootstrap';
+import '../App.css'
+
+const Navigation = () => {
+    return(
+        <Navbar expand="lg" sticky="top" className="page-nav">
+            <Container fluid>
+                <Navbar.Brand href="#">
+                    <img
+                        src="./images/white-logo.png"
+                        width="160"
+                        height="60"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto page-links"
+                    // style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link href="" className="text-link">Search</Nav.Link>
+                </Nav>
+                <Button variant="success">
+                    Log In / Register
+                </Button>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+    )
+}
+
+export default Navigation;
