db62b4cb836ff1937d6d48d7173a3d0e272578c7
Bill Song <songchao456@gmail.com>
2021-10-26 20:19:57 -0400

    add map border

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -86,7 +86,7 @@ class SearchPage extends Component {
                     googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
                     loadingElement={<div style={{ height: `100%` }} />}
                     containerElement={<div style={{ height: `100%` }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
+                    mapElement={<div style={{ height: `100%`, border: `3px solid black` }} />}
                 >
                 </Map>
             </div>
