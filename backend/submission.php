27215b73ffcd4b8c92ebcdbfa8f401dcff7c7c65
Franklin Tian <franklin.tian2009@gmail.com>
2021-11-29 17:04:58 -0500

    added structure for submission php

--- backend/submission.php
+++ backend/submission.php
@@ -4,10 +4,9 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 $postdata = file_get_contents("php://input");
 
-$servername = "localhost";
-$username = "root";
+$servername = "";
+$username = "";
 $sqlpassword = "";
-$database= "bike4joy";
 
 $request = json_decode($postdata);
 
@@ -18,19 +17,63 @@ $description = $request->description;
 $longitude = $request->longitude;
 $latitude = $request->latitude;
  
-// Create connection
-$db = mysqli_connect($servername, $username, $sqlpassword, $database);
- 
+try {
+  $pdo = new PDO("mysql:host=$servername;dbname=bike4joy", $username, $sqlpassword);
+  // set the PDO error mode to exception
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
 
-$query = "INSERT INTO reviews VALUES('$type','$capacity','$rating','$description','$longitude','$latitude')";
-$stmt = $db->prepare($query);
-$stmt->execute();
+  //The email we are looking for.
+  $emailToLookFor = $email;
 
-if(mysqli_query($db, $query)){
-  echo "Data has been inserted successfully";
-}else{
-  echo "Error entering data to database ";
-  echo $email;
-  echo $db->error;
+  //The SQL query.
+  $query = "SELECT COUNT(*) AS num FROM USERS WHERE email = :email";
+
+  //Prepare the SQL statement.
+  $stmt = $pdo->prepare($query);
+
+  //Bind our email value to the :email parameter.
+  $stmt->bindValue(':email', $emailToLookFor);
+
+  //Execute the statement.
+  $stmt->execute();
+
+  //Fetch the row / result.
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  //If num is bigger than 0, the email already exists.
+  if($row['num'] > 0){
+   echo 'Row exists!';
+
+   //Find id corresponding to existing user
+   $emailToLookFor2 = $email;
+   $userid = "SELECT id FROM USERS WHERE email = :emailcheck";
+   $stmt2 = $pdo->prepare($userid);
+   $stmt2->bindValue(':emailcheck', $emailToLookFor2);
+   $stmt2->execute();
+   $realUserId = $stmt2->fetch(PDO::FETCH_ASSOC)["id"];
+   echo $realUserId; //<---Returned userid
+
+  } else{//Else the email doesnt exist.
+    echo 'Row does not exist!';
+    $sql = "INSERT INTO USERS VALUES(0, '$type', '$capacity','$rating', '$description', '$longitude', '$latitude')";
+    // use exec() because no results are returned
+    $pdo->exec($sql);
+    echo " New record created successfully";
+   //Find id corresponding to existing user
+   $emailToLookFor2 = $email;
+   $userid = "SELECT id FROM USERS WHERE email = :emailcheck";
+   $stmt2 = $pdo->prepare($userid);
+   $stmt2->bindValue(':emailcheck', $emailToLookFor2);
+   $stmt2->execute();
+   $realUserId = $stmt2->fetch(PDO::FETCH_ASSOC)["id"];
+   echo $realUserId; //<---Returned userid
+  } 
+
+
+
+
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 ?>
