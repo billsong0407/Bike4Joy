180ac3e0de01a03504a83771b4244ba3c24931c2
Bill Song <songchao456@gmail.com>
2021-11-30 21:41:16 -0500

    fetch location data

--- client/src/pages/SingleResult.js
+++ client/src/pages/SingleResult.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Navigation from '../components/navbar';
 import Footer from '../components/footer';
 import Map from '../components/map';
+import axios from 'axios';
 import { Container, Row, Col, Table, Card, Button } from 'react-bootstrap';
 
 import "../css/single-result-page.css";
@@ -46,10 +47,7 @@ class SingleResultPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // state data
-            data: [
-                { "type": "Feature", "properties": { "_id": 5056, "ADDRESS_POINT_ID": 30069161, "ADDRESS_NUMBER": "35", "LINEAR_NAME_FULL": "York St", "ADDRESS_FULL": "35 York St", "POSTAL_CODE": "M5J 0C7", "MUNICIPALITY": "former Toronto", "CITY": "Toronto", "WARD": "Spadina-Fort York (10)", "PLACE_NAME": null, "GENERAL_USE_CODE": 115001, "CENTRELINE_ID": 13975053, "LO_NUM": 35, "LO_NUM_SUF": null, "HI_NUM": null, "HI_NUM_SUF": null, "LINEAR_NAME_ID": 4735, "X": null, "Y": null, "LONGITUDE": null, "LATITUDE": null, "ID": 54, "PARKING_TYPE": "Angled Bike Rack", "FLANKING": "Front St W", "BICYCLE_CAPACITY": 30, "SIZE_M": 2.9, "YEAR_INSTALLED": 2015, "BY_LAW": null, "DETAILS": null, "OBJECTID": 25 }, "geometry": { "type": "Point", "coordinates": [ -79.381465101411194, 43.643790673159501 ] } }
-            ],
+            data: [],
             reviews: [
                 {"image": "/images/p1.jpg", "video": "/videos/samplevideo.mp4", "rating:": "Ratings: ★★★★☆", "comment": "Racks are in good quality", "author": "Bob Leung"},
                 {"image": "", "video": "/videos/samplevideo.mp4", "rating:": "Ratings: ★★☆☆☆", "comment": "Many abandoned bikes are taking the spots", "author": "Jasper Percy"},
@@ -57,6 +55,31 @@ class SingleResultPage extends Component {
             ],
         }
     }
+
+    componentDidMount(props){
+        let param = this.props.match.params.id
+        let param_val = param.split('=')[1]
+        this.setState({
+            locID: param_val
+        })
+        this.getLocation(param_val)
+        this.getReviews(param_val)
+    }
+
+    getLocation(locID){
+        console.log(locID)
+        axios.get("http://127.0.0.1:8000/api/location/getByID.php", {params: {id: locID}})
+        .then(res => {
+            const location = res.data.results
+            console.log(location)
+            this.setState({ data: location });
+        })
+    }
+
+    getReviews(locID){
+        // console.log(locID)
+    }
+
     render(){
         return (
             <>
@@ -65,44 +88,57 @@ class SingleResultPage extends Component {
                     <Row className="d-flex align-items-center justify-content-center">
                         <Col className="animate__animated animate__slideInLeft single-map-section">
                             {/* Load Google Maps */}
-                            <Map
-                                lat={this.state.data[0].geometry.coordinates[1]}
-                                lng={this.state.data[0].geometry.coordinates[0]}
-                                zoom={14}
+                            {this.state.data.length > 0 &&
+                            (<Map
+                                lat={this.state.data[0].lat}
+                                lng={this.state.data[0].lng}
+                                zoom={12}
+                                showLink={true}
                                 mapData={this.state.data}
                                 googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
                                 loadingElement={<div style={{ height: `100%` }} />}
                                 containerElement={<div style={{ height: `100%` }} />}
                                 mapElement={<div style={{ height: `100%` }} />}
-                            ></Map>
+                                >
+                            </Map>)}
+                            {this.state.data.length <= 0 &&
+                                (<Map
+                                lat={43.6532}
+                                lng={-79.3832}
+                                zoom={12}
+                                showLink={true}
+                                mapData={this.state.data}
+                                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+                                loadingElement={<div style={{ height: `100%` }} />}
+                                containerElement={<div style={{ height: `100%` }} />}
+                                mapElement={<div style={{ height: `100%` }} />}
+                            >
+                            </Map>)}
                         </Col>
                         {/* Display Tabular information */}
+                        {this.state.data.length > 0 && (
                         <Col className="animate__animated animate__slideInRight col-auto single-info">
                             <h2>Information</h2>
                             <Table hover responsive>
                                 <tbody>
                                 <tr>
                                     <td className="first-column">Address:</td>
-                                    <td>{this.state.data[0].properties.ADDRESS_FULL}, {this.state.data[0].properties.CITY}</td>
+                                    <td>{this.state.data[0].ADDRESS}</td>
                                 </tr>
                                 <tr>
                                     <td className="first-column">Postal Code:</td>
-                                    <td>{this.state.data[0].properties.POSTAL_CODE}</td>
+                                    <td>{this.state.data[0].POSTAL_CODE}</td>
                                 </tr>
                                 <tr>
                                     <td className="first-column">Type:</td>
-                                    <td>{this.state.data[0].properties.PARKING_TYPE}</td>
+                                    <td>{this.state.data[0].PARKING_TYPE}</td>
                                 </tr>
                                 <tr>
                                     <td className="first-column">Bicycle Capacity</td>
-                                    <td>{this.state.data[0].properties.BICYCLE_CAPACITY}</td>
+                                    <td>{this.state.data[0].BICYCLE_CAPACITY}</td>
                                 </tr>
                                 <tr>
-                                    <td className="first-column">Year Installed:</td>
-                                    <td>2015</td>
-                                </tr>
-                                <tr>
-                        {/* link to location details (single object page)  */}
+                                    {/* link to location details (single object page)  */}
                                     <td></td>
                                     <td><Button href="/submission" className="btn-success"
                                     aria-pressed="true">submit a review</Button></td>
@@ -110,6 +146,7 @@ class SingleResultPage extends Component {
                                 </tbody>
                             </Table>
                         </Col>
+                        )}
                     </Row>
                     {/* Show reviews and corresponding ratings */}
                     <div className="py-5">
